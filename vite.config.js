f8bc3174989019dee6fbaf8e3bb3fe06868d8514
Luis Fernando <54nch0luis@gmail.com>
2025-07-31 00:13:40 -0300

    correção config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/mini-kambam/',
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  base:"/mini-kambam"
+  ]
 })
